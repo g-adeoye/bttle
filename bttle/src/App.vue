a4e7c56a1ee9cde4e975a97bd1c807dcbeeb6cc8
gideon <gdeon822@gmail.com>
2026-04-01 11:11:10 +0200

    feat: add button component

--- bttle/src/App.vue
+++ bttle/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+</script>
 
 <template>
   <h1 class="text-red-500 font-extrabold">You did it!</h1>
@@ -6,6 +8,12 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="min-h-screen min-w-screen flex items-center justify-center">
+    <Button variant="outline">
+      Click me
+    </Button>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
