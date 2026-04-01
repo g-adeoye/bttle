4be74282528678918baf47f3d6de7fafbb117f46
gideon <gdeon822@gmail.com>
2026-04-01 10:27:37 +0200

    first commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/bttle
+
+go 1.26.1
---
+++ bttle/tailwind.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./index.html", "./src/**/*.{vue,ts,tsx}"],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: []
+}
